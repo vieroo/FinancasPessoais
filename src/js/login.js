2e15c7aeaa9719acb0b7731a2f2491773517e109
Eduardo Viero <duduviero@gmail.com>
2023-11-22 20:22:13 -0300

    Criacao Login

--- src/js/login.js
+++ src/js/login.js
@@ -2,10 +2,21 @@ document.getElementById('doLogin').addEventListener('click', () => {
   let username = document.getElementById('username').value
   let password = document.getElementById('password').value
 
-  if (username === 'admin' && password === 'admin') {
-    alert('Login efetuado com sucesso')
-    window.location.href='home.html'
-  } else {
-    alert('Usuario ou senha incorretos')
-  }
+  console.log(username)
+  console.log(password)
+
+  getLogin(username, password)
 })
+
+async function getLogin(username, password) {
+  fetch('http://localhost:3000/users')
+  .then(response => response.json())
+  .then(users => {
+    const user = users.find(u => u.login === username && u.password == password)
+    if (user) {
+      window.location.href='home.html'
+    } else {
+      alert('Usuario ou senha incorretos')
+    }
+  })
+}
