5cd464c5ece93ae371318e7d5cf9c098c8cbdf07
Eduardo Viero <duduviero@gmail.com>
2023-11-27 19:25:41 -0300

    Criacao da funcao para salvar transacao

--- src/js/financas.js
+++ src/js/financas.js
@@ -1,19 +1,46 @@
-document.getElementById('save-transaction').addEventListener('onclick', (ev) => {
-  ev.preventDefault();
+let transactions = []
 
+
+// Função para salvar as transações
+async function saveTransaction(ev) {
+  ev.preventDefault()
+
+  const transactionID = document.getElementById('transaction-id')
   const transactionName = document.getElementById('transaction-name').value
-  const transactionValue = document.getElementById('transaction-value').value
+  const transactionValue = parseFloat(document.getElementById('transaction-value').value)
   const transactionType = document.querySelector('input[name="transaction-type"]::checked').id
 
-  const newTransaction = {
-    name: transactionName,
-    value: transactionValue,
-    type: transactionType
+
+  // Verifica se já existe transacao, se tiver atualiza o valor
+  if (transactionID) {
+    const response = await fetch(`http://localhost:3000/transactions/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({ transactionName, transactionValue, transactionType}),
+      headers: {
+        'Content-Type': 'Application/json'
+      }
+    })
+    const transaction = await response.json()
+    const indexToRemove = transactions.findIndex((t) => t.index === transactionID)
+    transactions.splice(indexToRemove, 1, transaction)
+    document.querySelector(`#transaction=${transactionID}`).remove()
+    //renderTransaction(transaction) // faltar criar essa funcao
+
+  } else {
+    // Criar uma nova transacao
+    const response = await fetch('http://localhost:3000/transactions', {
+      method: 'POST',
+      body: JSON.stringify({ transactionName, transactionValue, transactionType}),
+      headers: {
+        'Content-Type': 'Application/json'
+      }
+    })
+
+    const transaction = await response.json()
+    transactions.push(transaction)
+    //renderTransaction(transaction)
   }
 
-  addNewTransaction(newTransaction)
-})
-
-async function addNewTransaction(newTransaction) {
- await fetch('http://lolcalhost:3000/transaction')
+  ev.target.reset()
+  //updateBalance() // Atualizar o valor do saldo, falta criar ainda
 }
