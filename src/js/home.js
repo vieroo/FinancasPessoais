58c96c1299eee494c697a951bef8cfa2244bf4f6
Extremezada <rafa.dalmagro@hotmail.com>
2023-11-28 19:37:30 -0300

    Adicionando arquivo de alteração de cor do nome do usuário.

--- src/js/home.js
+++ src/js/home.js
@@ -1,17 +1,20 @@
 const userNameChange = () =>{
     
     let login = localStorage.getItem("username")
-
     if(login){
         let username = document.getElementById('entrar')
 
         username.onmouseover = () => {
             username.style.pointerEvents = 'none'
         }
-        
+        username.classList.remove('text-light')
+        username.classList.add('text-warning')
         username.innerHTML = login
+        
     }
 }
+
+
 
 const clearLocalStorage = () => {
     localStorage.removeItem('username')
